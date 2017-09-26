78d0f415b82391c3d1957c32d367582968e5dbcb
Josh Getter <getterj@mail.gvsu.edu>
2017-09-26 19:31:05 -0400

    Initial

--- ServerControl/main.cpp
+++ ServerControl/main.cpp
@@ -5,11 +5,31 @@
 //  Created by Josh Getter on 9/22/17.
 //  Copyright © 2017 Josh Getter. All rights reserved.
 //
-
+#include <string>
 #include <iostream>
 
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string input;
+    while(1){
+        cout << "Enter a value\n";
+        getline(cin,input);
+        if(input.find("createServer") != -1){
+            cout << "Creating Server\n";
+            //create server
+        }
+    }
     return 0;
 }
+class Server{
+    string serverName;
+    int minProcs;
+    int maxProcs;
+    int numActive;
+    
+    Server(string _serverName, int _minProcs, int _maxProcs){
+        serverName = _serverName;
+        minProcs = _minProcs;
+        maxProcs = _maxProcs;
+    }
+};
