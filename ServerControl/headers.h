13a1db54cf058d69e483387911d1dc74f705dc25
Josh Getter <getterj@mail.gvsu.edu>
2017-09-29 17:01:17 -0400

    Parent, Child, and Grandchildren now create successfully.

---
+++ ServerControl/headers.h
@@ -0,0 +1,13 @@
+//
+//  headers.h
+//  ServerControl
+//
+//  Created by Josh Getter on 9/27/17.
+//  Copyright © 2017 Josh Getter. All rights reserved.
+//
+
+#ifndef headers_h
+#define headers_h
+
+
+#endif /* headers_h */
--- ServerControl/main.cpp
+++ ServerControl/main.cpp
@@ -12,8 +12,16 @@
 #include <sstream>
 #include <vector>
 #include <stdlib.h>
+#include <pthread.h>
 
 using namespace std;
+
+class Server;
+class ServerController;
+pid_t rootPid;
+const string CONTROLLER = "[CONTROLLER] ";
+const string CHILD = "[CHILD] ";
+const string GRANDCHILD = "[GRANDCHILD] ";
 
 class Server{
 public:
@@ -22,16 +30,43 @@ public:
     int maxProcs;
     int numActive;
     pid_t pid;
-    
-    Server(int _minProcs, int _maxProcs, string _serverName){
+    vector<pid_t> processes;
+    Server(string _serverName, int _minProcs, int _maxProcs){
         serverName = _serverName;
         minProcs = _minProcs;
         maxProcs = _maxProcs;
         numActive = 0;
-        pid = getpid(); //Should be updated/initialized after fork
+        pid_t _pid = fork();
+        if(!_pid){
+            //child level process so create grand-children
+            cout << CHILD + "I am main server process\n";
+            pid = getpid();
+            //Update instance with childs pid
+            createProcess(minProcs);
+            //Finished creating grand-children now wait for signal
+            pause();
+        }
+    }
+    void createProcess(int count){
+        if(getpid() == pid){
+            for(int created = 1; created <= count; created++){
+                //Is child level server so replicate
+                pid_t grandChildPid;
+                grandChildPid = fork();
+                if(grandChildPid){
+                    //Still parent server
+                    //Add newly created grand - child.
+                    processes.push_back(grandChildPid);
+                }else{
+                    //is grand child
+                    cout << GRANDCHILD + "I am server instance\n";
+                    pause();
+                }
+                
+            }
+        }
     }
 };
-
 
 class ServerController{
 private:
@@ -39,25 +74,27 @@ private:
     void start(){
         string input;
         while(1){
-            cout << "Enter a value\n";
-            getline(cin,input);
-            istringstream inputStream(input);
-            vector<string> tokens;
-            string token;
-            while(getline(inputStream,token, ' ')){
-                //Tokenize line
-                tokens.push_back(token);
-            }
-            if(tokens[0] == "createServer"){
-                cout << "Creating Server\n";
-                //create server
-                if(tokens.size() != sizeof(4)){
-                    cout << "Invalid parameters for createServer";
-                }else{
-                    Server currServer = Server(atoi((tokens[1].c_str())), atoi(tokens[2].c_str()), tokens[3]);
-                    serverMap.insert(pair<string,Server>(currServer.serverName,currServer));
+            if(getpid() == rootPid){
+                cout << CONTROLLER + "Enter a value\n";
+                getline(cin,input);
+                istringstream inputStream(input);
+                vector<string> tokens;
+                string token;
+                while(getline(inputStream,token, ' ')){
+                    tokens.push_back(token);
+                }
+                if(tokens[0] == "createServer"){
+                    cout << CONTROLLER + "Creating Server\n";
+                    //create server
+                    if(tokens.size() != sizeof(4)){
+                        cout << CONTROLLER + "Invalid parameters for createServer\n";
+                    }else{
+                        Server currServer = Server(tokens[1], atoi((tokens[2].c_str())), atoi(tokens[3].c_str()));
+                        serverMap.insert(pair<string,Server>(tokens[1], currServer));
+                    }
                 }
             }
+            
         }
     };
 public:
@@ -65,8 +102,8 @@ public:
         start();
     }
 };
-
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
+    rootPid = getpid();
     ServerController();
     return 0;
 }
