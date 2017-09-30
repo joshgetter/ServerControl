e1a415c069c63945f6620318c7e631d6b727daf9
Josh Getter <getterj@mail.gvsu.edu>
2017-09-30 13:39:03 -0400

    Adds createProcess command
    * implements createProcess command
    * sets up logic for abort process command

--- ServerControl/main.cpp
+++ ServerControl/main.cpp
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/prctl.h>
+#include <signal.h>
 
 using namespace std;
 
@@ -23,49 +24,91 @@ pid_t rootPid;
 const string CONTROLLER = "[CONTROLLER] ";
 const string CHILD = "[CHILD] ";
 const string GRANDCHILD = "[GRANDCHILD] ";
+const int ABORTSERV = 0;
+const int CREATEPROC = 1;
+const int ABORTPROC = 2;
+Server *serverInstance;
 
 class Server{
 public:
     string serverName;
     int minProcs;
     int maxProcs;
-    int numActive;
     pid_t pid;
     vector<pid_t> processes;
+    struct sigaction createProcSigAction;
+    struct sigaction abortProcSigAction;
     Server(string _serverName, int _minProcs, int _maxProcs){
         serverName = _serverName;
         minProcs = _minProcs;
         maxProcs = _maxProcs;
-        numActive = 0;
+        //Register child/grand-child signal handlers
+        createProcSigAction.sa_sigaction = incrementProcess;
+        abortProcSigAction.sa_sigaction = decrementProcess;
+        sigaction(SIGRTMIN+CREATEPROC, &createProcSigAction, NULL);
+        sigaction(SIGRTMIN+ABORTPROC, &abortProcSigAction, NULL);
         pid_t _pid = fork();
         if(!_pid){
             //child level process so create grand-children
             cout << CHILD + "I am main server process\n";
             pid = getpid();
-            prctl(PR_SET_NAME, serverName.c_str(),NULL,NULL,NULL);
+            //Set process name
+            prctl(PR_SET_NAME, serverName.substr(0,15).c_str(),NULL,NULL,NULL);
             //Update instance with childs pid
             createProcess(minProcs);
-            //Finished creating grand-children now wait for signal
-            pause();
+            //Finished creating grand-children now wait for signal and record reference to self.
+            serverInstance = this;
+            doNothing();
+        }else{
+            //Controller instance, update controllers version with PID.
+            pid = _pid;
         }
     }
-    void createProcess(int count){
+    static void incrementProcess(int sigNum, siginfo_t * sigInfo, void * context){
+        serverInstance->incrementProcess(1);
+    }
+    static void decrementProcess(int sigNum, siginfo_t * sigInfo, void * context){
+        serverInstance->incrementProcess(-1);
+    }
+    void createProcess(int num, bool exceedBound = false){
         if(getpid() == pid){
-            for(int created = 1; created <= count; created++){
-                //Is child level server so replicate
-                pid_t grandChildPid;
-                grandChildPid = fork();
-                if(grandChildPid){
-                    //Still parent server
-                    //Add newly created grand-child.
-                    processes.push_back(grandChildPid);
-                }else{
-                    //is grand child
-                    cout << GRANDCHILD + "I am server instance\n";
-                    pause();
+            //Is child level server so continue operation
+            if(num > 0){
+                //Creating processes
+                for(int created = 0; created < num; created++){
+                    pid_t grandChildPid = fork();
+                    if(grandChildPid){
+                        //Still child level server
+                        //Add newly created grand-child.
+                        processes.push_back(grandChildPid);
+                    }else{
+                        //is grand child
+                        cout << GRANDCHILD + "I am server instance\n";
+                        doNothing();
+                    }
                 }
-                
+            }else{
+                //removing processes
+                for(int removed = 0; removed > num; removed--){
+                    pid_t removingPid = processes.back();
+                    kill(SIGINT, removingPid);
+                    processes.pop_back();
+                }
             }
+
+        }
+    }
+    void incrementProcess(int inc){
+        cout << CHILD << "Num processes, minprocs, maxprocs " << processes.size() << ", " << minProcs << ", " << maxProcs << "\n";
+        if(processes.size() + size_t(inc) >= size_t(minProcs) && processes.size() + size_t(inc) <= size_t(maxProcs)){
+            createProcess(inc);
+        }else{
+            cout << CHILD << "Cannot perform operation, number of processes will be out of bounds\n";
+        }
+    }
+    void doNothing(){
+        while(1){
+            pause();
         }
     }
 };
@@ -73,6 +116,7 @@ public:
 class ServerController{
 private:
     map<string, Server> serverMap;
+    struct sigaction abortServSigAction;
     void start(){
         string input;
         while(1){
@@ -86,11 +130,13 @@ private:
                     tokens.push_back(token);
                 }
                 if(tokens[0] == "createServer"){
-                    cout << CONTROLLER + "Creating Server\n";
                     //create server
-                    if(tokens.size() != sizeof(4)){
+                    if(tokens.size() != size_t(4)){
                         cout << CONTROLLER + "Invalid parameters for createServer\n";
+                    }else if(serverMap.count(tokens[1]) >= size_t(1)){
+                        cout << CONTROLLER << "That server already exists.\n";
                     }else{
+                        cout << CONTROLLER + "Creating Server\n";
                         Server currServer = Server(tokens[1], atoi((tokens[2].c_str())), atoi(tokens[3].c_str()));
                         serverMap.insert(pair<string,Server>(tokens[1], currServer));
                     }
@@ -102,6 +148,15 @@ private:
                     rPid = output.str();
                     string cmd = "ps f -o pid,comm -g $(ps -o sid= -p " + rPid + ")";
                     system(cmd.c_str());
+                }
+                if(tokens[0] == "createProcess"){
+                    if(tokens.size() != size_t(2)){
+                        cout << CONTROLLER << "Invalid parameters for createProcess\n";
+                    }else if(serverMap.count(tokens[1]) != size_t(1)){
+                        cout << CONTROLLER << "That server doesn't exist\n";
+                    }else{
+                        kill(serverMap.at(tokens[1]).pid, SIGRTMIN + CREATEPROC);
+                    }
                 }
             }
             
